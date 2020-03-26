3f618893432cf154afb585273c42f28e8120e696
Samuel Phua <samuel.phua@jaga-me.com>
2020-03-26 15:38:17 +0800

    added graphql to utils folder.

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,4 @@
-export * from "./array";
-export * from "./context";
-export * from "./sequelize";
+export * from './array';
+export * from './context';
+export * from './sequelize';
+export * from './graphql';
